687c42c74f42775e88c1dbb59f9469a3a37b77c2
Young <54241472+anonymousingh@users.noreply.github.com>
2022-03-10 02:55:52 +0000

    Add files via upload

---
+++ Tutorial/tut.py
@@ -0,0 +1,45 @@
+print('''*  *   *   *   *   *   *   *   *   *   *   *   *   *   *   *   *
+         *                                                              *
+         *  W   E   L   C   O   M   E                                   *
+         *                                                              *
+         *                           T  O                               *
+         *                I.T                                           *
+         *  G   R   A   D   I   N   G                                   *
+         *                              S   Y   S   T   E   M           *
+         *                                                              *
+         *                                                              *
+         *  *   *   *   *   *   *   *   *   *   *   *   *   *   *   *   *''')
+
+name = input('Enter your name: ')
+
+
+grade = ['A', 'B+', 'B', 'C+', 'C', 'D+', 'D', 'E']
+
+print("Hello", name.upper())
+
+score = input("Enter your score: ")
+if score >= '80':
+    print("Excellent! Your score grade is", grade[0])
+
+elif '75' <= score <= '79':
+    print("Very good! Your score grade is", grade[1])
+
+elif '70' <= score <= '74':
+    print("Good! Your score grade is", grade[2])
+
+elif '65' <= score <= '69':
+    print("Your score grade is", grade[3], 'Satisfactory')
+
+elif '60' <= score <= '64':
+    print("Your score grade is", grade[4], 'Pass')
+
+elif '55' <= score <= '59':
+    print("Your score grade is", grade[5], 'Weak pass')
+
+elif '50' <= score <= '54':
+    print("Your score grade is", grade[6], 'Poor')
+
+else:
+    print("Your score grade is", grade[7], 'Fail')
+
+
